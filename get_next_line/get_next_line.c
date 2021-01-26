542b337e6fe986034541ff725fc6e72176b09efe
Luca Marzano <lmarzano@e4r9p3.42roma.it>
2021-01-26 18:59:34 +0100

    wip2

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,26 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 11:19:48 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/01/26 12:23:34 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/01/26 18:54:37 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int get_next_line(int fd, char **line)
+int	get_next_line(int fd, char **line)
 {
-    charbuffer[BUFFER_SIZE + 1];
-    
-    if (fd < 0 || line == NULL)
+	char		buffer[BUFFER_SIZE + 1];
+	char		*tmp;
+	static char	*s[4096];
+	int			i;
+
+	if (fd < 0 || line == NULL)
 		return (-1);
 }
 /*
 read() attempts to read nbyte bytes of data from the object referenced by the descriptor fildes into the buffer pointed to by buf.
 
-If successful, the number of bytes actually read is returned.  Upon reading end-of-file, zero is returned.  Otherwise, a -1 is 
+If successful, the number of bytes actually read is returned.  Upon reading end-of-file, zero is returned.  Otherwise, a -1 is
 returned and the global variable errno is set to indicate the error.
 
 
