690e951ce84adf9e0a50b2cf135b62f3d15b88bd
Luca Marzano <lmarzano@e3r11p1.42roma.it>
2021-01-27 17:35:03 +0100

    wip3

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/23 10:46:55 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/01/23 11:16:24 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/01/27 16:25:09 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@
 # include <string.h>
 # include <unistd.h>
 
-int	get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line);
+int		just_do_it(char **line, char *buffer);
+
+size_t	ft_strlen(const char *s);
+char	*ft_realloc(char **line);
 
 #endif
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,46 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/23 10:46:37 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/01/27 17:24:21 by lmarzano         ###   ########.fr       */
+/*   Created: 2021/01/27 11:00:28 by lmarzano          #+#    #+#             */
+/*   Updated: 2021/01/27 16:33:08 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
 
-size_t	ft_strlen(const char *s)
+int	main(void)
 {
-	size_t	i;
+	int		fd;
+	char	*line;
 
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_realloc(char **line)
-{
-	char	*new;
-	int		i;
-
-	i = 0;
-	new = malloc(ft_strlen(*line) + BUFFER_SIZE + 1);
-	if (*line)
-	{
-		while ((*line)[i])
-		{
-			new[i] = (*line)[i];
-			i++;
-		}
-		free(*line);
-		*line = NULL;
-	}
-	new[i] = '\0';
-	return (new);
+	line = 0;
+	fd = open("file1", O_RDONLY);
+/*
+** 1st line
+*/
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+/*
+** 2nd line
+*/
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+/*
+** 3rd line
+*/
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+/*
+** 4th line
+*/
+	get_next_line(fd, &line);
+	printf("%s\n", line);
 }
