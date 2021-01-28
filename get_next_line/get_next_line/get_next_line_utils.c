ca0477365f3ceb82a5338c099020c7778f8c8061
Luca Marzano <lmarzano@student.42roma.it>
2021-01-28 17:05:14 +0100

    GNL PLATINUM OK MOK

---
+++ get_next_line/get_next_line/get_next_line_utils.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/23 10:46:37 by lmarzano          #+#    #+#             */
+/*   Updated: 2021/01/28 17:03:34 by lmarzano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_realloc(char **line)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	if (!(new = malloc(ft_strlen(*line) + BUFFER_SIZE + 1)))
+		return (NULL);
+	if (*line)
+	{
+		while ((*line)[i])
+		{
+			new[i] = (*line)[i];
+			i++;
+		}
+		free(*line);
+		*line = NULL;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+char	*ft_realloc_finale(char **line)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	if (!(new = malloc(ft_strlen(*line)+ 1)))
+		return (NULL);
+	if (*line)
+	{
+		while ((*line)[i])
+		{
+			new[i] = (*line)[i];
+			i++;
+		}
+		free(*line);
+		*line = NULL;
+	}
+	new[i] = '\0';
+	return (new);
+}
