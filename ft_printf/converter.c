8a447888125a3f1892663d52e448dd896068f020
Luca Marzano <lmarzano@student.42roma.it>
2021-02-03 14:00:15 +0100

    added itoa to converters

--- ft_printf/converter.c
+++ ft_printf/converter.c
@@ -1,25 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   converter.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/01 11:09:18 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/02/03 12:04:41 by lmarzano         ###   ########.fr       */
+/*   Created: 2021/02/03 12:02:33 by lmarzano          #+#    #+#             */
+/*   Updated: 2021/02/03 13:59:46 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-char	ft_putchar(int c, t_format *carrier)
+char	*ft_itoa(int n)
 {
-	write(1, &c, 1);
-	carrier->count++;
-}
+	long	n;
+	size_t	len;
+	char	*res;
 
-int		ft_printf(const char *fstr, ...)
-{
-	if (!fstr || !*fstr)
+	n = n;
+	len = (n > 0) ? 0 : 1;
+	n = (n > 0) ? n : -n;
+	while (n)
+		n = len++ ? n / 10 : n / 10;
+	if (!(res = (char *)malloc(sizeof(char) * len + 1)))
 		return (NULL);
+	*(res + len--) = '\0';
+	while (n > 0)
+	{
+		*(res + len--) = n % 10 + '0';
+		n = n / 10;
+	}
+	if (len == 0 && res[1] == '\0')
+		*(res + len) = '0';
+	if (len == 0 && res[1] != '\0')
+		*(res + len) = '-';
+	return (res);
 }
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,19 +6,20 @@
 /*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 11:09:01 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/02/03 11:10:25 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/02/03 12:04:28 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef	struct	s_format
 {
-	/* data */
+	int	count;
 }				t_format;
 
-
-int	ft_printf(const char *fstr, ...);
+int				ft_printf(const char *fstr, ...);
 
 #endif
