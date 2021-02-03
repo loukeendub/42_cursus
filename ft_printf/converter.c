d61f14cad192f548d12d95d7d328c6e6e3c33108
Luca Marzano <lmarzano@student.42roma.it>
2021-02-03 14:10:41 +0100

    created tools.c

--- ft_printf/converter.c
+++ ft_printf/converter.c
@@ -6,7 +6,7 @@
 /*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 12:02:33 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/02/03 13:59:46 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/02/03 14:06:50 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,33 @@ char	*ft_itoa(int n)
 	if (len == 0 && res[1] != '\0')
 		*(res + len) = '-';
 	return (res);
+}
+
+int		ft_atoi(const char *str)
+{
+	int	sign;
+	int	i;
+	int	n;
+
+	i = 0;
+	sign = 1;
+	n = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == 45 || str[i] == 43)
+	{
+		if (str[i] == 45)
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		n *= 10;
+		n += (str[i] - 48);
+		i++;
+	}
+	if (n < 0 && n > -2147483648)
+		return (sign > 0 ? -1 : 0);
+	n = n * sign;
+	return (n);
 }
--- ft_printf/tools.c
+++ ft_printf/tools.c
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   tools.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/01 11:09:18 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/02/03 14:10:17 by lmarzano         ###   ########.fr       */
+/*   Created: 2021/02/03 14:03:45 by lmarzano          #+#    #+#             */
+/*   Updated: 2021/02/03 14:04:25 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int		ft_printf(const char *fstr, ...)
+char	ft_putchar(int c, t_format *carrier)
 {
-	if (!fstr || !*fstr)
-		return (NULL);
+	write(1, &c, 1);
+	carrier->count++;
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
