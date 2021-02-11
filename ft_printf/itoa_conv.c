0e4041053a8d0fd78a6495a2f41a9a8257c5928e
Luca Marzano <lmarzano@student.42roma.it>
2021-02-11 18:40:15 +0100

    added few comments for tomorrow

--- ft_printf/itoa_conv.c
+++ ft_printf/itoa_conv.c
@@ -6,7 +6,7 @@
 /*   By: lmarzano <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 12:02:33 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/02/11 18:25:09 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/02/11 18:39:59 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,16 @@ char	hex_conv(int n)
 	size_t	len;
 	char	*tmp;
 
+//put conversion from int to hex here!
+/*
+** Divide the decimal number by 16. Treat the division as an integer division.
+** Write down the remainder (in hexadecimal).
+** Divide the result again by 16. Treat the division as an integer division.
+** Repeat step 2 and 3 until result is 0.
+** The hex value is the digit sequence of the remainders from the last to first.
+*/
+//use ternary operator to handle caps below
+//g_carrier->type == 'X' ? "0123456789ABCDEF": "0123456789abcdefg";
 
 	if (g_carrier->type == 'X')
 		while (tmp && *tmp)
