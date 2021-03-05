fad3914f425c1a3e8848bb8fafd52a213e12d892
Luca Marzano <lmarzano@student.42roma.it>
2021-03-05 13:40:43 +0100

    practicing on inter-union for Exam Rank 02

--- ExamPractice/myunion.c
+++ ExamPractice/myunion.c
@@ -5,37 +5,57 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/05 12:17:00 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/03/05 12:31:34 by lmarzano         ###   ########.fr       */
+/*   Created: 2021/03/04 10:45:35 by lmarzano          #+#    #+#             */
+/*   Updated: 2021/03/04 14:57:17 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	char			inventory[256] = {0};
-	unsigned char	c;
-	int				shelf;
-	int				item;
+	int		i;
+	int		j;
+	int		used;
 
+	i = 0;
 	if (argc == 3)
 	{
-		shelf = 1;
-		while (shelf < 3)
+		while (argv[1][i])
 		{
-			item = 0;
-			while (argv[shelf][item])
+			used = 0;
+			j = 0;
+			while (j < i)
 			{
-				c = argv[shelf][item];
-				if (!inventory[c])
-				{
-					inventory[c] = 1;
-					write(1, &c, 1);
-				}
-				item++;
+				if (argv[1][j] == argv[1][i])
+					used = 1;
+				j++;
 			}
-			shelf++;
+			if (!used)
+				write(1, &argv[1][i], 1);
+			i++;
+		}
+		i = 0;
+		while (argv[2][i])
+		{
+			used = 0;
+			j = 0;
+			while (argv[1][j])
+			{
+				if (argv[1][j] == argv[2][i])
+					used = 1;
+				j++;
+			}
+			j = 0;
+			while (j < i)
+			{
+				if (argv[2][j] == argv[2][i])
+					used = 1;
+				j++;
+			}
+			if (!used)
+				write(1, &argv[2][i], 1);
+			i++;
 		}
 	}
 	write(1, "\n", 1);
--- ExamPractice/union-inter/union01.c
+++ ExamPractice/union-inter/union01.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inter.c                                            :+:      :+:    :+:   */
+/*   union01.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/03 14:15:13 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/03/05 13:40:40 by lmarzano         ###   ########.fr       */
+/*   Created: 2021/03/05 13:11:48 by lmarzano          #+#    #+#             */
+/*   Updated: 2021/03/05 13:39:41 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,33 @@
 
 int	main(int argc, char **argv)
 {
-	char	inventory[256] = {0};
-	int		shelf;
-	int		item;
-	unsigned char c;
+	char			inventory[256] = {0};
+	unsigned char	c;
+	int				shelf;
+	int				item;
 
 	if (argc == 3)
 	{
-		shelf = 2;
-		while (shelf > 0)
+		shelf = 1;
+		while (shelf < 3)
 		{
 			item = 0;
 			while (argv[shelf][item])
 			{
 				c = argv[shelf][item];
-				if (shelf == 2 && !inventory[c])
+				if (!inventory[c])
 				{
+					write(1, &c, 1);
 					inventory[c] = 1;
-				}
-				else if (shelf == 1 && inventory[c])
-				{
-					write(1, &argv[c], 1);
-					inventory[c] = 2;
 				}
 				item++;
 			}
-			shelf--;
+			shelf++;
 		}
 	}
 	write(1, "\n", 1);
 	return (0);
 }
+
+// time	: 7m20s
+// error: 1. = line 30 "c = argv[shelf][item]"
