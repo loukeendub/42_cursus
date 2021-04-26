c104c7e5345c6b943223815c2f05576d36b1e649
Luca Marzano <lmarzano@student.42roma.it>
2021-04-26 12:18:47 +0200

    implemented metallb in setup.sh and added yaml config file into srcs

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,12 +1,21 @@
+# see what changes would be made, returns nonzero returncode if different
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl diff -f - -n kube-system
 
+# actually apply the changes, returns nonzero returncode on errors only
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 docker build ./srcs/nginx -t nginx
 docker build ./srcs/phpmyadmin -t phpmyadmin
 docker build ./srcs/wordpress -t wordpress
 docker build ./srcs/mysql -t mysql
+
+kubectl apply -f mlb_conf.yaml
