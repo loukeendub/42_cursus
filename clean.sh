1b8ca3af5f937679da95df8072b088813c494727
Luca Marzano <lmarzano@student.42roma.it>
2021-09-22 13:06:21 +0200

    clean.sh

---
+++ clean.sh
@@ -0,0 +1,10 @@
+rm -rf ~/Library/Caches
+rm -rf ~/Library/Application\ Support/Slack/Cache
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage
+rm -rf ~/Library/Application\ Support/Code/Crashpad
+rm -rf ~/Library/Application\ Support/Code/Cache
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage
+rm -rf ~/Library/Application\ Support/Code/CachedData
+rm -rf ~/Library/Application\ Support/Chrome/Default
+rm -rf ~/Library/Developer/CoreSimulator
+rm -rf ~/Library/Containers/com.docker.docker
